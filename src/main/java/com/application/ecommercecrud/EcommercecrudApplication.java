1e9cda2d99d882397c284f3f169e807605fd4cfa
Paramesh T S <parameshts19@gmail.com>
2025-09-08 21:08:31 +0530

    initialize Spring repo

---
+++ src/main/java/com/application/ecommercecrud/EcommercecrudApplication.java
@@ -0,0 +1,13 @@
+package com.application.ecommercecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercecrudApplication.class, args);
+	}
+
+}
